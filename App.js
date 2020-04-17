d648fb4c90d4fd15005853903e455166d59391ed
Yana Shetko <mac@YanasMacBookPro.local>
2020-04-17 21:08:08 +0300

    second

--- App.js
+++ App.js
@@ -1,19 +1,134 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Image, TouchableOpacity, Alert} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start workinmg   your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
+export default class  App extends Component {
+  render() {
+   
+    return (
+      <View style={styles.MainContainer}>
+         <TouchableOpacity activeOpacity={0.5}
+           onPress={() => Alert.alert('4th button pressed')}
+           >
+             <Image 
+               source={require('./assets/man.png')} 
+               style={{ height: 75, width: 65, marginBottom: 0}}  
+             />
+         </TouchableOpacity>       
+ 
+         <View 
+           style={styles.ViewButtons}
+           >
+               <TouchableOpacity style={styles.ButtonStyle} activeOpacity={0.5}
+                 onPress={() => Alert.alert('4th button pressed')}
+                 >
+                   <Image 
+                     source={require('./assets/pic1.png')} 
+                    style={styles.ImageIconStyle} 
+                    // style={{ height: 68, width: 65, margin: 1,  }} 
+                   /> 
+               </TouchableOpacity>
+ 
+               <TouchableOpacity style={styles.ButtonStyle} activeOpacity={0.5}
+                 onPress={() => Alert.alert('4th button pressed')}
+                 >
+                   <Image 
+                     source={require('./assets/pic2.png')} 
+                     style={styles.ImageIconStyle} 
+                   /> 
+               </TouchableOpacity>
+ 
+         </View>
+ 
+         <View 
+           style={styles.ViewButtons}
+           >
+               <TouchableOpacity style={styles.ButtonStyle} activeOpacity={0.5}
+                 onPress={() => Alert.alert('4th button pressed')}
+                 >
+                   <Image 
+                     source={require('./assets/pic3.png')} 
+                    style={styles.ImageIconStyle} 
+                    // style={{ height: 68, width: 65, margin: 1,  }} 
+                   /> 
+               </TouchableOpacity>
+ 
+               <TouchableOpacity style={styles.ButtonStyle} activeOpacity={0.5}
+                 onPress={() => Alert.alert('4th button pressed')}
+                 >
+                   <Image 
+                     source={require('./assets/pic4.png')} 
+                     style={styles.ImageIconStyle} 
+                   /> 
+               </TouchableOpacity>
+ 
+         </View>
+ 
+         <View 
+           style={styles.ViewButtons}
+           >
+               <TouchableOpacity style={styles.ButtonStyle} activeOpacity={0.5}
+                 onPress={() => Alert.alert('4th button pressed')}
+                 >
+                   <Image 
+                     source={require('./assets/pic5.png')} 
+                    style={styles.ImageIconStyle} 
+                    // style={{ height: 68, width: 65, margin: 1,  }} 
+                   /> 
+               </TouchableOpacity>
+ 
+               <TouchableOpacity style={styles.ButtonStyle} activeOpacity={0.5}
+                 onPress={() => Alert.alert('4th button pressed')}
+                 >
+                   <Image 
+                     source={require('./assets/pic6.png')} 
+                     style={styles.ImageIconStyle} 
+                   /> 
+               </TouchableOpacity>
+ 
+         </View>
+ 
+ 
+      </View>
+    );
+  }
+ }
+ 
+ 
+ const styles = StyleSheet.create({
+ 
+  MainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center',
+    margin: 0,
+    backgroundColor: 'black',
+ },
+ 
+ ViewButtons: {
+   flexDirection: 'row',
+   alignItems: 'center',
+ },
+ 
+ ButtonStyle: {
+   flexDirection: 'row',
+   alignItems: 'center',
+   backgroundColor: '#d97102',
+   borderColor: '#d97102',
+   height: 125,
+   width: 125, 
+   borderRadius: 14,
+   margin: 13,
+   justifyContent: 'space-around',
+ },
+ 
+ ImageIconStyle: {
+    height: 70,
+    width: 70,
+    resizeMode : 'stretch',
+ //borderWidth: .5,
+ //borderColor: 'red',
+ 
+ }, 
+ });
+ 
+  
